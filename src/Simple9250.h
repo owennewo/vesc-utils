1d348a83b440bb3f622302cfaa1637289074f122
Owen Williams <williams.owen@gmail.com>
2021-03-21 20:09:22 +0000

    adding imu

---
+++ src/Simple9250.h
@@ -0,0 +1,23 @@
+#ifndef __SIMPLE_9250_H__
+#define __SIMPLE_9250_H__
+#include <SparkFunMPU9250-DMP.h>
+
+
+struct euler_angles_t {
+    float roll; 
+    float pitch;
+    float yaw;
+    long dt_micros; // time since last sample
+};
+
+class Simple9250 {
+    public:
+        Simple9250();
+        int resetI2C(int sda_pin, int scl_pin);
+        void begin(unsigned short sampleRate);
+        bool readData(euler_angles_t* angles);
+    private:
+        MPU9250_DMP* imu;
+};
+
+#endif
